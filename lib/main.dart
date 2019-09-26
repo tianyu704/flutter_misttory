ef3f381ab9cf393291309926c638f886d31705b0
tianyu704 <839629712@qq.com>
2019-09-26 10:57:32 +0800

    create porject

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:amap_base_location/amap_base_location.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  AMapLocation _aMapLocation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _aMapLocation = AMapLocation();
+    LocationClientOptions options = LocationClientOptions(
+      locationMode: LocationMode.Battery_Saving,
+      interval: 5000,
+    );
+    _aMapLocation.startLocate(options).listen((Location location) {
+      print(location.toJson());
+    });
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
