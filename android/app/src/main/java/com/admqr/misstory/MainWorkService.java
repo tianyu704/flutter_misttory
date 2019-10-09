0a1828947367b0637e8cb2ac060a0d6d2ad0ffdd
tianyu704 <839629712@qq.com>
2019-10-09 16:16:16 +0800

    在service里存储Location

--- android/app/src/main/java/com/admqr/misstory/MainWorkService.java
+++ android/app/src/main/java/com/admqr/misstory/MainWorkService.java
@@ -1,15 +1,20 @@
 package com.admqr.misstory;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Messenger;
 import android.util.Log;
 
+import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.shihoo.daemon.work.AbsWorkService;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
@@ -53,7 +58,7 @@ public class MainWorkService extends AbsWorkService {
 
     @Override
     public void onServiceKilled() {
-        saveData();
+//        saveData();
         Log.d("wsh-daemon", "onServiceKilled --- 保存数据到磁盘");
     }
 
@@ -63,7 +68,7 @@ public class MainWorkService extends AbsWorkService {
         if (mDisposable != null && !mDisposable.isDisposed()) {
             mDisposable.dispose();
         }
-        saveData();
+//        saveData();
     }
 
     @Override
@@ -73,15 +78,16 @@ public class MainWorkService extends AbsWorkService {
             //设置定位回调监听
             mLocationClient.setLocationListener(aMapLocation -> {
                 Log.e("android", aMapLocation.toStr());
-                saveData();
+                saveData(aMapLocation);
             });
         }
         if (!mLocationClient.isStarted()) {
             AMapLocationClientOption option = new AMapLocationClientOption();
             option.setInterval(1000 * 60 * 5);
+            option.setDeviceModeDistanceFilter(1000);
             option.setLocationMode(AMapLocationClientOption.AMapLocationMode.Battery_Saving);
             mLocationClient.setLocationOption(option);
-//            mLocationClient.startLocation();
+            mLocationClient.startLocation();
         }
 //        Log.d("wsh-daemon", "检查磁盘中是否有上次销毁时保存的数据");
 //        mDisposable = Observable
@@ -108,13 +114,47 @@ public class MainWorkService extends AbsWorkService {
     }
 
 
-    private void saveData() {
-        long stamp = System.currentTimeMillis() / 1000;
-        if (Math.abs(mSaveDataStamp - stamp) >= 3) {
-            // 处理业务逻辑
-            Log.d("wsh-daemon", "保存数据到磁盘。");
+    private void saveData(AMapLocation aMapLocation) {
+        String path = getDir("data", MODE_PRIVATE).getAbsolutePath();
+        File pathFile = new File(path);
+        if (pathFile.exists()) {
+            File file = new File(path + "/Misstory");
+            SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(file, null);
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("altitude", aMapLocation.getAltitude());
+            contentValues.put("speed", aMapLocation.getSpeed());
+            contentValues.put("bearing", aMapLocation.getBearing());
+            contentValues.put("citycode", aMapLocation.getCityCode());
+            contentValues.put("adcode", aMapLocation.getAdCode());
+            contentValues.put("country", aMapLocation.getCountry());
+            contentValues.put("province", aMapLocation.getProvince());
+            contentValues.put("city", aMapLocation.getCity());
+            contentValues.put("district", aMapLocation.getDistrict());
+            contentValues.put("road", aMapLocation.getRoad());
+            contentValues.put("street", aMapLocation.getStreet());
+            contentValues.put("number", aMapLocation.getStreetNum());
+            contentValues.put("poiname", aMapLocation.getPoiName());
+            contentValues.put("errorCode", aMapLocation.getErrorCode());
+            contentValues.put("errorInfo", aMapLocation.getErrorInfo());
+            contentValues.put("locationType", aMapLocation.getLocationType());
+            contentValues.put("locationDetail", aMapLocation.getLocationDetail());
+            contentValues.put("aoiname", aMapLocation.getAoiName());
+            contentValues.put("address", aMapLocation.getAddress());
+            contentValues.put("poiid", aMapLocation.getBuildingId());
+            contentValues.put("floor", aMapLocation.getFloor());
+            contentValues.put("description", aMapLocation.getDescription());
+            contentValues.put("time", aMapLocation.getTime());
+            contentValues.put("provider", aMapLocation.getProvider());
+            contentValues.put("lon", aMapLocation.getLongitude());
+            contentValues.put("lat", aMapLocation.getLatitude());
+            contentValues.put("accuracy", aMapLocation.getAccuracy());
+            contentValues.put("isOffset", aMapLocation.isOffset());
+            contentValues.put("isFixLastLocation", aMapLocation.isFixLastLocation());
+            contentValues.put("coordType", aMapLocation.getCoordType());
+            contentValues.put("is_delete", false);
+            db.insert("MSLocation", null, contentValues);
+            db.close();
         }
-        mSaveDataStamp = stamp;
     }
 
 }
