9b179dbc267d134bc0668addd462cee1dab1871f
tianyu704 <839629712@qq.com>
2019-10-15 17:03:24 +0800

    悬停demo

--- lib/pages/pois_page.dart
+++ lib/pages/pois_page.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 ///
 class PoisPage extends StatelessWidget {
   final String json;
+  TextEditingController _controller;
+  ScrollController _scrollController = ScrollController();
 
   PoisPage(this.json);
 
@@ -16,7 +18,58 @@ class PoisPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Pois"),
       ),
-      body: Text(json),
+      body: CustomScrollView(
+        controller: _scrollController,
+
+        slivers: <Widget>[
+          //AppBar，包含一个导航栏
+          SliverAppBar(
+            pinned: true,
+            elevation: 0,
+            floating: true,
+            bottom: PreferredSize(
+              child: Container(
+                color: Colors.red,
+                height: 100,
+              ),
+              preferredSize: Size(double.infinity, 100),
+            ),
+            centerTitle: true,
+            expandedHeight: 250,
+            leading: Text(""),
+            backgroundColor: Colors.white,
+            flexibleSpace: FlexibleSpaceBar(
+              collapseMode: CollapseMode.pin,
+              background: Container(
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: TextField(
+                    maxLines: 5,
+                    scrollPhysics: BouncingScrollPhysics(),
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          //List
+          new SliverFixedExtentList(
+            itemExtent: 50.0,
+            delegate: new SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                //创建列表项
+                return new Container(
+                  alignment: Alignment.center,
+                  color: Colors.lightBlue[100 * (index % 9)],
+                  child: new Text('list item $index'),
+                );
+              },
+              childCount: 50, //50个列表项
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
