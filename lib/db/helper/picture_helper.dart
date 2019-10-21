d1fe6af4e14c52d01dd25252c30612ff44ad28a8
tianyu704 <839629712@qq.com>
2019-10-21 10:55:28 +0800

    创建Picturehelper

---
+++ lib/db/helper/picture_helper.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+///
+/// Create by Hugo.Guo
+/// Date: 2019-10-18
+///
+class PictureHelper {
+  static final PictureHelper _instance = PictureHelper._internal();
+
+  factory PictureHelper() => _instance;
+
+  PictureHelper._internal();
+
+}
